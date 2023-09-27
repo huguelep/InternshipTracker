71286227b0a1bac88ee69143ab83040fe9c3ff11
Patrick A Huguelet <97004839+huguelep@users.noreply.github.com>
2023-09-27 19:57:39 -0400

    Add files via upload

---
+++ db/Controllers/ApplicationController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JobApplicationTracker.Models;
+
+namespace JobApplicationTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Application
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Application>>> GetApplications()
+        {
+          if (_context.Applications == null)
+          {
+              return NotFound();
+          }
+            return await _context.Applications.ToListAsync();
+        }
+
+        // GET: api/Application/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Application>> GetApplication(int id)
+        {
+          if (_context.Applications == null)
+          {
+              return NotFound();
+          }
+            var application = await _context.Applications.FindAsync(id);
+
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return application;
+        }
+
+        // PUT: api/Application/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplication(int id, Application application)
+        {
+            if (id != application.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(application).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Application
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Application>> PostApplication(Application application)
+        {
+          if (_context.Applications == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Applications'  is null.");
+          }
+            _context.Applications.Add(application);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetApplication", new { id = application.Id }, application);
+        }
+
+        // DELETE: api/Application/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplication(int id)
+        {
+            if (_context.Applications == null)
+            {
+                return NotFound();
+            }
+            var application = await _context.Applications.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            _context.Applications.Remove(application);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ApplicationExists(int id)
+        {
+            return (_context.Applications?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ db/Models/Application.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobApplicationTracker.Models
+{
+    public class Application
+    {
+        [Key]
+        public int Id {  get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Company { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string Link { get; set; } = string.Empty;
+
+        [Column(TypeName = "datetime")]
+        public DateTime DateApplied { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string ApplicationStatus { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ResumeVersion { get; set; } = string.Empty;
+
+    }
+}
